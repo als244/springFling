046dd50693044fde3cf21c3476147d08b00e3672
als244 <andrew.sheinberg@yale.edu>
2019-10-20 15:31:11 -0400

    changes

--- springFling/LaunchViewController.swift
+++ springFling/LaunchViewController.swift
@@ -3,10 +3,6 @@
 //  Yale Spring Fling
 //
 //  Created by Andrew Sheinberg on 9/9/19.
-<<<<<<< HEAD
-=======
-//  Copyright © 2019 Project T. All rights reserved.
->>>>>>> 2c2490b83bcddd4a965b64bc84eb04b93f2a9301
 //
 
 import UIKit
