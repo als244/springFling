b751ccba9f8f39f4fe45a389ddd540788dc1fa6a
als244 <andrew.sheinberg@yale.edu>
2019-10-20 13:58:04 -0400

    initial git commit

---
+++ springFling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  springFling
+//
+//  Created by Andrew Sheinberg on 10/20/19.
+//  Copyright © 2019 Andrew Sheinberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
